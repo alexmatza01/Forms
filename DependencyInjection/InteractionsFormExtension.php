9a591778ccd425e46c91c115e1cc019519cea388
Mataoanu Alex <alexmatza01>
2018-04-02 13:13:52 +0300

    Update services

---
+++ DependencyInjection/InteractionsFormExtension.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 4/2/2018
+ * Time: 1:05 PM
+ */
+
+namespace Interactions\FormBundle\DependencyInjection;
+
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class InteractionsFormExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/../Resources/config')
+        );
+        $loader->load('services.yaml');
+    }
+}
